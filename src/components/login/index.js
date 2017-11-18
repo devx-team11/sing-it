d9203d70d5789fec3f196c7eff3262fab2cd56bc
abenezer <abenezerabebe@gmail.com>
2017-11-18 22:49:56 +0100

    update form button to work with client side scripts from demo

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import SPOTIFYAUTHURL from '../../config';
 
 export default () => (
-  <a href={SPOTIFYAUTHURL}>Login</a>
+  <button className="btn btn-lg btn-primary" onClick={window.Demo.sendToLogin}>Log in with Spotify</button>
 );
