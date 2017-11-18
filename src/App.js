dde1a10a36ccac77e1a31fec87dbc6eee3a3b5b5
abenezer <abenezerabebe@gmail.com>
2017-11-18 15:25:01 +0100

    Update app with form component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Form } from './components';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Welcome to Sing-it</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Form handleChange={this.handleChange} handleSubmit={this.handleSubmit} inputValue={this.state.inputValue} />
       </div>
     );
   }
