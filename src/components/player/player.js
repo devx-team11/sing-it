074bd39f11216a74e5beb366749e2d5100efdffc
abenezer <abenezerabebe@gmail.com>
2017-11-19 10:18:03 +0100

    Update player to have cropping tool and handle cropping changes

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -5,12 +5,23 @@ import PlayerTrack from './player-track';
 import PlayerControls from './controls';
 import PlayerArtists from './artists';
 import PlayerBackgroundAlbumArt from './background-album-art';
+import Slider from '../slider';
 
 export default class Player extends Component {
+  handleCroppingChange ([beginPercentage, endPercentage]) {
+      window.Demo.WebPlaybackSDK.getCurrentState().then(res => {
+        const { duration_ms } = res.track_window.current_track
+        const snippetBeginning = Math.floor((duration_ms / 100) * beginPercentage)
+        const snippetEnding = Math.floor((duration_ms / 100) * endPercentage)
+        window.Demo.WebPlaybackSDK.seek().then(res => {
+          snippetBeginning;
+          window.Demo.WebPlaybackSDK.resume();
+        });
+      });
+  }
   render() {
     let { current_track } = this.props.currentState.track_window;
     let image = current_track.album.images[2];
-    // let input = this.state.value || '';
     return (
       <div className="screen screen-player">
         <div className="player">
@@ -19,8 +30,8 @@ export default class Player extends Component {
               <PlayerAlbumArt image_url={image.url} />
             </div>
             <div className="col-sm-9">
-              <PlayerProgress state={this.props.currentState} showPosition={true} showDuration={true} />
               <PlayerTrack track={current_track} />
+              <Slider handleChange={this.handleCroppingChange} />
               <PlayerArtists artists={current_track.artists} />
               <PlayerControls state={this.props.currentState} />
             </div>
