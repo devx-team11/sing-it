6f7a35b480651d6bb15b8a5cfeb99544e192a0fb
abenezer <abenezerabebe@gmail.com>
2017-11-19 09:42:56 +0100

    merge conflict

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Form } from './form';
 export { default as SpotifyLogin } from './login';
 export { default as Slider } from './slider';
+export { default as Player } from './player';
