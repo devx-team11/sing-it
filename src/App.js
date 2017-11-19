7c9262146b1cd6bfeaa3348777d815f80efd96e5
abenezer <abenezerabebe@gmail.com>
2017-11-19 09:44:13 +0100

    Merge conflict fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ReactDOM from 'react-dom';
+
 import logo from './logo.svg';
 import { Form, SpotifyLogin, Player, Slider } from './components';
 import { parseQuery } from './util';
 import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import ReactDOM from 'react-dom';
 
 class App extends Component {
   constructor () {
@@ -24,15 +25,8 @@ class App extends Component {
   handleSubmit () {
     alert(`Finding song ${this.state.inputValue}`);
   }
-  handleSliderChange (changeArray) {
-    this.setState(state => ({
-      snippetBeginning: changeArray[0],
-      snippetEnd: changeArray[1]
-    }))
-  }
   shouldRenderPlayer () {
     setTimeout(() => {
-      console.log(window.Demo.onSpotifyPlayerConnected);
       this.setState({ renderPlayer: window.Demo.onSpotifyPlayerConnected})
     }, 2000);
   }
@@ -40,35 +34,15 @@ class App extends Component {
     this.shouldRenderPlayer();
   }
   render() {
-    // Demo.onSpotifyUserSessionExpires = () => {
-    //   Demo.WebPlaybackSDK.disconnect(); // Disconnect the player
-
-    //   ReactDOM.render(
-    //     <div>
-    //       <PlayerError
-    //         heading="Session expired."
-    //         message="Playback sessions only last 60 minutes. Refresh for new session." />
-    //       <Authorize />
-    //     </div>,
-    //     document.getElementById('screen')
-    //   );
-    // };
-
-    // Demo.renderWebPlaybackSDKError = (title, e) => {
-    //   ReactDOM.render(
-    //     <PlayerError heading={title} message={e} />,
-    //     document.getElementById('screen')
-    //   );
-    // };
     return (
-    <MuiThemeProvider>
-      <div className='App'>
-        <Slider handleChange={this.handleSliderChange}/>
-        {window.Demo.isAccessToken() === false && <SpotifyLogin />}
-        <Form handleChange={this.handleFormChange} handleSubmit={this.handleSubmit} inputValue={this.state.inputValue} />
-        { this.state.renderPlayer && <Player /> }
-      </div>
-    </MuiThemeProvider>
+      <MuiThemeProvider>
+        <div className='App'>
+          <Slider handleChange={this.handleSliderChange}/>
+          {window.Demo.isAccessToken() === false && <SpotifyLogin />}
+          <Form handleChange={this.handleFormChange} handleSubmit={this.handleSubmit} inputValue={this.state.inputValue} />
+          { this.state.renderPlayer && <Player /> }
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
