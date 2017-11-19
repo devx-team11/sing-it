45563454ea78c305e34d92fbac40c721657af95c
abenezer <abenezerabebe@gmail.com>
2017-11-19 01:42:25 +0100

    Make connect player available to app

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -3,20 +3,3 @@ import ConnectPlayer from './connect-player';
 import './index.css';
 
 export default () => <ConnectPlayer />
-
-// window.Demo.onSpotifyUserSessionExpires = () => {
-//   window.Demo.WebPlaybackSDK.disconnect(); // Disconnect the player
-
-// {/* <div>
-//   <PlayerError
-//     heading="Session expired."
-//     message="Playback sessions only last 60 minutes. Refresh for new session." />
-//   <Authorize />
-// </div> */}
-// };
-
-// // window.Demo.renderWebPlaybackSDKError = (title, e) => {
-// //   ReactDOM.render(
-// //     <PlayerError heading={title} message={e} />,
-// //   );
-// // };
