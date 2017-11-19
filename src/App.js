794fb949193040f2e2ce94dc493a2a4ec8a14ae0
abenezer <abenezerabebe@gmail.com>
2017-11-19 09:41:02 +0100

    Merge conflict

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import { Form, SpotifyLogin, Slider } from './components';
+import { Form, SpotifyLogin, Player, Slider } from './components';
 import { parseQuery } from './util';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ReactDOM from 'react-dom';
-
-
-
 
 class App extends Component {
   constructor () {
@@ -17,37 +14,59 @@ class App extends Component {
       snippetBeginning: 0,
       snippetEnd: 100,
     }
-    // this.handleFormChange = this.handleFormChange.bind(this);
-    // this.handleSliderChange = this.handleSliderChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.handleSliderChange = this.handleSliderChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleFormChange = (e) => {
+  handleFormChange (e) {
     this.setState({ inputValue: e.target.value });
   }
-  handleSubmit = () => {
+  handleSubmit () {
     alert(`Finding song ${this.state.inputValue}`);
   }
-  handleSliderChange = (changeArray) => {
+  handleSliderChange (changeArray) {
     this.setState(state => ({
       snippetBeginning: changeArray[0],
       snippetEnd: changeArray[1]
     }))
   }
+  shouldRenderPlayer () {
+    setTimeout(() => {
+      console.log(window.Demo.onSpotifyPlayerConnected);
+      this.setState({ renderPlayer: window.Demo.onSpotifyPlayerConnected})
+    }, 2000);
+  }
   componentDidMount () {
-    if (window.location.pathname === '/callback') {
-      localStorage.setItem('spotify-token', parseQuery(window.location, 'hash').access_token);
-    }
+    this.shouldRenderPlayer();
   }
   render() {
+    // Demo.onSpotifyUserSessionExpires = () => {
+    //   Demo.WebPlaybackSDK.disconnect(); // Disconnect the player
+
+    //   ReactDOM.render(
+    //     <div>
+    //       <PlayerError
+    //         heading="Session expired."
+    //         message="Playback sessions only last 60 minutes. Refresh for new session." />
+    //       <Authorize />
+    //     </div>,
+    //     document.getElementById('screen')
+    //   );
+    // };
+
+    // Demo.renderWebPlaybackSDKError = (title, e) => {
+    //   ReactDOM.render(
+    //     <PlayerError heading={title} message={e} />,
+    //     document.getElementById('screen')
+    //   );
+    // };
     return (
     <MuiThemeProvider>
       <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Welcome to Sing-it</h1>
-        </header>
-        <SpotifyLogin />
-        <Form handleChange={this.handleFormChange} handleSubmit={this.handleSubmit} inputValue={this.state.inputValue} />
         <Slider handleChange={this.handleSliderChange}/>
+        {window.Demo.isAccessToken() === false && <SpotifyLogin />}
+        <Form handleChange={this.handleFormChange} handleSubmit={this.handleSubmit} inputValue={this.state.inputValue} />
+        { this.state.renderPlayer && <Player /> }
       </div>
     </MuiThemeProvider>
     );
@@ -55,3 +74,4 @@ class App extends Component {
 }
 
 export default App;
+
